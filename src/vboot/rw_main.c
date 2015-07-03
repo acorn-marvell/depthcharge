44a45f480dd916873f66b2bcd422d91c3bf5bc74
Benjamin Huang <gdhuang@marvell.com>
2015-07-03 03:13:14 -0700

    Add Cylone codes
    add Cyclone codes
    
    BUG=None
    TEST=None

--- src/vboot/rw_main.c
+++ src/vboot/rw_main.c
@@ -56,11 +56,6 @@ int main(void)
 
 	timestamp_add_now(TS_RW_VB_SELECT_AND_LOAD_KERNEL);
 
-	// Select a kernel and boot it.
-	if (storage_init()) {
-		storage_read(0, 100000, 0x20000000);
-	} else
- 		halt();
 	// We should never get here.
 	printf("Got to the end!\n");
 	halt();
