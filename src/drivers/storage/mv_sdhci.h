9cacc8a1caefcbaf7ad71a7b3e72ae6930fda38f
kfyao <kfyao@marvell.com>
2015-11-04 02:05:12 -0800

    Further refine the code to get ready for the upstreaming

--- src/drivers/storage/mv_sdhci.h
+++
@@ -1,7 +0,0 @@
-#ifndef __DRIVER_STORAGE_MV_SDHCI_H__
-#define __DRIVER_STORAGE_MV_SDHCI_H__
-SdhciHost *new_mv_sdhci_host(unsigned int regbase, unsigned int clock_min, unsigned int clock_max, unsigned int quirks);
-#define INTER_REGS_BASE         0xF1000000
-#define MV_SDMMC_REGS_OFFSET            (0xD8000)
-#define CONFIG_SYS_MMC_BASE                    (INTER_REGS_BASE + MV_SDMMC_REGS_OFFSET)
-#endif
